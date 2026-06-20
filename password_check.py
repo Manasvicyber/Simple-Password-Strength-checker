1dfc428521d1d29d096655a8d7f0952c195b890a
Manasvicyber <manasvi.shrivastava07@gmail.com>
2026-06-20 14:43:35 +0530

    Add password strength checking function

---
+++ password_check.py
@@ -0,0 +1,11 @@
+def check_password_strength(password):
+  if len(password) < 8:
+    return "Weak: Password must be atleast 8 characters long." 
+
+  has_number = any(char.isdigit() for char in password) 
+  has_upper = any(char.isupper() for char in password)
+
+  if has_number and has_upper:
+    return "Strong Password!"
+  else:
+    return "Moderate: Try adding capital letters and numbers."
