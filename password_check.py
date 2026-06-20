28a4afaf9279c0955e673698e1b62d638b44c020
Manasvicyber <manasvi.shrivastava07@gmail.com>
2026-06-20 15:00:30 +0530

    Update password_check.py

--- password_check.py
+++ password_check.py
@@ -1,11 +1,16 @@
 def check_password_strength(password):
-  if len(password) < 8:
-    return "Weak: Password must be atleast 8 characters long." 
+    if len(password) < 8:
+      return "Weak: Password must be atleast 8 characters long." 
 
-  has_number = any(char.isdigit() for char in password) 
-  has_upper = any(char.isupper() for char in password)
+    has_number = any(char.isdigit() for char in password) 
+    has_upper = any(char.isupper() for char in password)
 
-  if has_number and has_upper:
-    return "Strong Password!"
-  else:
-    return "Moderate: Try adding capital letters and numbers."
+    if has_number and has_upper:
+      return "Strong Password!"
+    else:
+      return "Moderate: Try adding capital letters and numbers."
+
+
+
+user_password = input("Enter a password to test: ")
+print(check_password_strength(user_password))
